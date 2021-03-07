9416d2bc61c0467cccc7004078fa64ec32fd64e0
Nathaniel Shirts <nshirts7@gmail.com>
2021-03-06 21:28:37 -0700

    fixed id number in DevList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import DevList from './DevList';
-=======
-import './App.css';
-import Header from './components/Header';
-import NavBar from './components/NavBar';
->>>>>>> creating-home-page
 
 export default class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <div>
         <header>
           <span class="logo">WRPT 2 GITHUB GROUP</span>
           <nav>
-              <ul class="nav-links">
-                  <li>Home</li>
-                  <li>Coming Soon</li>
-                  <li>Spacejam.com</li>
-              </ul>
+            <ul class="nav-links">
+              <li>Home</li>
+              <li>Coming Soon</li>
+              <li>Spacejam.com</li>
+            </ul>
           </nav>
         </header>
         <main>
@@ -27,16 +20,10 @@ export default class App extends Component {
             <DevList />
           </section>
         </main>
-          <footer>
-              <div class="copyright">Produced By Devmountain Students</div>
-          </footer>    
+        <footer>
+          <div class="copyright">Produced By Devmountain Students</div>
+        </footer>
       </div>
-=======
-      <>
-        <Header />
-        <NavBar />
-      </>
->>>>>>> creating-home-page
     );
   }
 }
